28101fd449808ff3c856343ea023ebc1027f43eb
vega-d <vegad0100110@gmail.com>
2022-09-05 23:08:52 +0200

    add wifi auto switch on wired connection

---
+++ auto-wifi-turn/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# this script is a part of Vega's install automation project, do not use by itself.
+
+cp ./misc/99-disable-wireless-when-wired /etc/NetworkManager/dispatcher.d/
