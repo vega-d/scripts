f810d5d4d2d569f3b605f9f5ccf42718371bbdfc
vega-d <vegad0100110@gmail.com>
2022-09-23 14:16:00 +0200

    added vfio support

--- virt-manager/install.sh
+++ virt-manager/install.sh
@@ -11,3 +11,15 @@ grep -E '^libvirt:' /usr/lib/group >> /etc/group
 usermod -aG libvirt vega
 EOF
 
+sudo cp ./misc/vfio /etc/modules-load.d/vfio
+sudo cp ./misc/vfio /etc/modprobe.d/vfio.conf
+
+sudo rpm-ostree kargs \
+--append-if-missing=intel_iommu=on \
+pcie_acs_override=downstream,multifunction \
+video=efifb:off \
+video=vesa:off \
+vfio-pci.ids=1002:67df,1002:aaf0 \
+vfio_iommu_type1.allow_unsafe_interrupts=1 \
+kvm.ignore_msrs=1
+
